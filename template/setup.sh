a704a7ddac27998468dc131e02e668b7c2279b9e
nalbam <me@nalbam.com>
2020-04-22 15:26:37 +0900

    keycloak

--- template/setup.sh
+++ template/setup.sh
@@ -21,6 +21,8 @@ pushd /home/ec2-user
 wget https://downloads.jboss.org/keycloak/${VERSION}/keycloak-${VERSION}.tar.gz
 tar -zxvf keycloak-${VERSION}.tar.gz
 
+# ./keycloak-${VERSION}/bin/add-user-keycloak.sh -r master -u <username> -p <password>
+
 ./keycloak-${VERSION}/bin/standalone.sh -b ${PRIVATE_IP}
 
 popd
@@ -28,8 +30,8 @@ popd
 # slack
 if [ "$TOKEN" != "" ]; then
 curl -sL opspresso.com/tools/slack | bash -s -- \
-    --token="$TOKEN" --username="gitlab" \
-    --footer_icon='https://about.gitlab.com/ico/favicon.ico' \
-    --footer="<https://$DNS_NAME|gitlab>" \
+    --token="$TOKEN" --username="keycloak" \
+    --footer_icon='https://www.keycloak.org/resources/favicon.ico' \
+    --footer="<https://$DNS_NAME|keycloak>" \
     --title="Launched Keycloak"
 fi
